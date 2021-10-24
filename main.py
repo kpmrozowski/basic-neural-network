663b5c1bbeb00998b58b63ca9b931bacd4048925
Kornel <kpmrozowski@gmail.com>
2021-10-24 00:18:29 +0000

    total error computing

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from math import exp, tanh
 from utils import read_csv_file
 
 class Network:
-    def __init__(self):
+    def __init__(self, training_inputs=None, training_outputs=None):
         self.layers_count = 0
         self.layers = []
+        self.training_inputs = training_inputs
+        self.training_outputs = training_outputs
 
     def add_first_hidden(self, neurons_count, inputs_count, bias = 0, weights_mat = None):
         self.layers.append(Layer(neurons_count, bias, weights_mat, inputs_count))
@@ -44,13 +46,24 @@ class Network:
     def train():
         pass
 
-    def total_error(self, training_sets):
+    def error(self, training_sets, output_sets):
         error = 0
         for t in range(len(training_sets)):
-            training_inputs, training_outputs = training_sets[t]
+            training_inputs = training_sets[t]
+            training_outputs = output_sets[t]
             self.inference(training_inputs)
             for o in range(len(training_outputs)):
-                error += self.layers[-1].neurons[o].calculate_error(training_outputs[o])
+                error += self.layers[-1].neurons[o].error(training_outputs[o])
+        return error
+
+    def total_error(self):
+        error = 0
+        for i in range(len(self.training_inputs)):
+            training_in = self.training_inputs[i]
+            training_out = self.training_outputs[i]
+            self.inference(training_in)
+            for o in range(len(training_out)):
+                error += self.layers[-1].neurons[o].error(training_out[o])
         return error
 
 
@@ -75,7 +88,6 @@ class Layer:
     def inference(self, inputs):
         self.inputs = inputs
         return [neuron.axon_output(inputs) for neuron in self.neurons]
-
 
 class Neuron:
     def __init__(self, weights, bias = 0, activation_function = 'sigmoid'):
@@ -111,13 +123,14 @@ class Neuron:
     
     # ∂E/∂zⱼ -->  the partial derivative of the neuron error with respect to the neuron input
     # δ = ∂E/∂zⱼ = ∂E/∂yⱼ * dyⱼ/dzⱼ
-    def DsynapsError_DsynapsInputsSum(self):
-        pass
+    def DsynapsError_DsynapsInputsSum(self, desired_output):
+        return self.DsynapsError_Doutput(desired_output) * self.Doutput_DsynapsInputsSum()
 
     # The partial derivative of the neuron error with respect to actual neuron output:
-    # ∂E/∂yⱼ = 2 * 0.5 * (target output - actual output) ^ (2 - 1) * -1 = -(tⱼ - yⱼ) = -(target output - actual output)
-    def DsynapsError_Doutput(self, target_output):
-        return -(target_output - self.output)
+    # ∂E/∂yⱼ = 2 * 0.5 * (desired output - actual output) ^ (2 - 1) * -1 = 
+    #        = -(tⱼ - yⱼ) = -(desired output - actual output)
+    def DsynapsError_Doutput(self, desired_output):
+        return -(desired_output - self.output)
 
     # The derivative of the neuron output with respect to the total network input
     #      yⱼ = 1 / (1 + e^(-zⱼ))  | d/dzⱼ
@@ -125,9 +138,16 @@ class Neuron:
     def Doutput_DsynapsInputsSum(self):
         return self.output * (1 - self.output)
 
+    # Each neuron error is calculated like this:
     # E = yⱼ* - yⱼ
-    def network_error(self, desired_output):
-        return 0.5 * (desired_output - self.output)
+    def error(self, desired_output):
+        return 0.5 * (desired_output - self.output) ** 2
+
+    # The partial derivative of the total net input with respective to a given 
+    #     weight (with everything else held constant):
+    # ∂zⱼ/∂wᵢ = some constant + 1 * xᵢw₁^(1-0) + some constant ... = xᵢ
+    def DsynapsInputsSum_Dweight(self, index):
+        return self.inputs[index]
 
 
 
@@ -145,7 +165,7 @@ def main():
                         help='name of file')
 
     parser.add_argument('-n', '--number', dest='number', type=str, choices=('100', '500', '1000', "10000"),
-                        default = "10000", help='count of samples')
+                        default = "100", help='count of samples')
 
     args = vars(parser.parse_args())
 
@@ -196,11 +216,14 @@ def main():
         train_cls = train_file.cls
         test_cls = test_file.cls
 
-    net = Network()
-    net.add_first_hidden(2, 1)
+    inputs = [[X_train[i], y_train[i]] for i in range(len(X_train))]
+    outputs = [[1., 0.] if c == 1 else [0., 1.] for c in train_cls]
+    net = Network(training_inputs=inputs, training_outputs=outputs)
+    net.add_first_hidden(10000, 2)
     net.add(2)
-    net.print_myself()
-    net.inference([1.])
+    # net.print_myself()
+    print("net.error({})={}".format([-0.432234621141106, 0.835330969654024], net.error([[1.]], [[0.8069052718966593, 0.7987629103901848]])))
+    print("net.total_error()={}".format(net.total_error()))
 
 
     
